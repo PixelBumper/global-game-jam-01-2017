6c35e07c2a558a3e95fb21d7d5083c087ec81234
pixelbumper <PixelBumper@users.noreply.github.com>
2017-01-18 23:21:35 +0100

    Initial commit with some utility to get going quicker

---
+++ Assets/Scripts/Utilities/AudioHelper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AudioHelper {
+
+    public static void PlayPitchAudio(AudioSource audioSource, AudioClip audioClip)
+    {
+        PlayPitchAudio(audioSource, audioClip, .7f, 1.3f);
+    }
+
+    public static void PlayPitchAudio(AudioSource audioSource, AudioClip audioClip, float minPitch, float maxPitch)
+    {
+        float randomPitch = Random.Range(minPitch, maxPitch);
+        audioSource.pitch = randomPitch;
+
+        audioSource.PlayOneShot(audioClip);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # global-game-jam-01-2017
+
+
+## Requirements
+Unity 5.5.0
+
+##libraries to save time
+
+Aron Granberg's A* library -> http://arongranberg.com/astar/download
+LeanTween tweening library -> https://github.com/dentedpixel/LeanTween or in the store

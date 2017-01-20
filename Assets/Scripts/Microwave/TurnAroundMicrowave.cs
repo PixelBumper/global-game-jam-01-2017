b7df0d81b71b092dab68a4f2c75996db4c877cf9
Sergio R. Lumley <lumley256@gmail.com>
2017-01-21 00:54:59 +0100

    Added microwaves with buttons that turn around

---
+++ Assets/Scripts/Microwave/TurnAroundMicrowave.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Microwave
+{
+    public class TurnAroundMicrowave : MonoBehaviour
+    {
+        public GameObject microwaveFront;
+        public GameObject microwaveBack;
+
+        public void FlipAround()
+        {
+            microwaveFront.SetActive(microwaveBack.activeSelf);
+            microwaveBack.SetActive(!microwaveBack.activeSelf);
+        }
+    }
+}

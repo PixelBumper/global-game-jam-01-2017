fe085d0fb3850602d5bf3c0557d42afe69a08c0c
Sergio R. Lumley <lumley256@gmail.com>
2017-01-21 16:34:08 +0100

    Creating inventory and initial key lock. Added key to inventory and game progress

--- Assets/Scripts/GameState/GameProgress.cs
+++ Assets/Scripts/GameState/GameProgress.cs
@@ -1,5 +1,23 @@
 ﻿// progress elements to indicate completion of areas in the game, cannot be removed
 public enum GameProgress
 {
-    LightUpColorMixPanel,
+    LightUpColorMixPanel, // This one is only for testing
+
+    // Actual game states (sorted by expected appearance)
+    TakenInitialPostIt,
+    OpenedChain,
+    TakenEnigmaPostIt,
+    ResolvedEnigmaPuzzle,
+    RemovedSlidingPuzzlePanel,
+    ResolvedSlidingPuzzle,
+    SkirimLockerOpened,
+    RemovedSimpleWiresPuzzlePanel,
+    TakenSimpleWiresPostIt,
+    ResolvedSimpleWiresPuzzle,
+    ResolvedSimonSaysPuzzle,
+    RemovedLeversPuzzlePanel,
+    ResolvedLevelsPuzzle,
+    ResolvedDoorOpen,
+    HamsterRescued,
+    HamsterExplode
 }

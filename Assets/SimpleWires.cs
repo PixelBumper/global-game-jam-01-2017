2f9f0f4f88ccf33a75a5ba2c4d580ce3c52a01f7
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-01-21 15:06:13 +0100

    Add scripts for wires

---
+++ Assets/SimpleWires.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleWires : MonoBehaviour {
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse Click SimpleWires");
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Test");
+
+                BoxCollider bc = hit.collider as BoxCollider;
+                if (bc != null)
+                {
+                    Destroy(bc.gameObject);
+                }
+            }
+        }
+    }
+}

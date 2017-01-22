a0e508463bd6abf15a3c08e37e00352cad1b6f93
Sergio R. Lumley <lumley256@gmail.com>
2017-01-22 02:31:56 +0100

    Added bolts to test scene

---
+++ Assets/Scripts/Puzzles/PanelBoltsPuzzle/PanelBoltsPuzzle.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelBoltsPuzzle : PuzzleModule {
+
+    public GameProgress ExpectedProgressToActivate;
+    public GameProgress GameProgressItUnlocks;
+
+    public List<SingleBoltPuzzle> bolts;
+    public int BoltsUnscrewed;
+
+    public bool IsInteractable;
+
+    // Use this for initialization
+    public override void OnPlayerProgress(GameProgress progress)
+    {
+        if (ExpectedProgressToActivate.Equals(progress))
+        {
+            MakeMeInteractable();
+            IsInteractable = true;
+        }
+    }
+
+    private void Start()
+    {
+        MakeMeInteractable();
+        foreach (var singleBoltPuzzle in bolts)
+        {
+            singleBoltPuzzle.ParentPanel = this;
+        }
+    }
+
+    public override GameProgress OwnGameProgressName
+    {
+        get { return GameProgressItUnlocks; }
+    }
+
+    public bool CanUnScreweBolt()
+    {
+        return AmIHolding(GameInventory.Screwdriver);
+    }
+
+    public void BoltUnscrewed()
+    {
+        BoltsUnscrewed++;
+        if (BoltsUnscrewed >= bolts.Count)
+        {
+            MarkAsSolved();
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Puzzles/PanelBoltsPuzzle/SingleBoltPuzzle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleBoltPuzzle : MonoBehaviour
+{
+
+    public PanelBoltsPuzzle ParentPanel;
+
+    private bool canUnScrewe = true;
+    public int MinScrewes = 2;
+    public int MaxScrewes = 7;
+    public int RequiredScrewes;
+    public int CurrentScrewes;
+
+    private void Start()
+    {
+        RequiredScrewes = Random.Range(MinScrewes, MaxScrewes);
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        if (ParentPanel && ParentPanel.CanUnScreweBolt() && canUnScrewe)
+        {
+            canUnScrewe = false;
+            CurrentScrewes++;
+            LeanTween.rotateZ(gameObject, gameObject.transform.rotation.z * Mathf.Rad2Deg + 90, 0.5f)
+            .setOnComplete(EnableScrewesAgain);
+        }
+    }
+
+    public void EnableScrewesAgain()
+    {
+        if (CurrentScrewes >= RequiredScrewes)
+        {
+            ParentPanel.BoltUnscrewed();
+            gameObject.SetActive(false);
+        }
+        canUnScrewe = true;
+    }
+}

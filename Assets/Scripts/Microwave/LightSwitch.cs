863c41ddc7eaa25bb1f2486decca546895663231
Sergio R. Lumley <lumley256@gmail.com>
2017-01-22 04:35:05 +0100

    Replaced light switch

--- Assets/Scripts/Microwave/LightSwitch.cs
+++ Assets/Scripts/Microwave/LightSwitch.cs
@@ -1,9 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
-public class LightSwitch : MonoBehaviour {
+public class LightSwitch : MonoBehaviour
+{
+
+    public List<GameObject> LightSwitches;
+
 
     private void OnMouseDown()
     {
         GameState.GetGlobalGameState().UnlockGameProgress(GameProgress.HamsterExplode);
+        foreach (var lightSwitch in LightSwitches)
+        {
+            lightSwitch.SetActive(!lightSwitch.activeSelf);
+        }
     }
 }
